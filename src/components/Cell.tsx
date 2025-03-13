301f734f93b21b3437974f6f0d9f2355597774e7
kinako825 <snowberries@gmail.com>
2025-03-13 21:29:45 +0900

    スプレッドシート

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,11 +1,32 @@
 import { CellContent } from "@/types/spreadsheet"
+import { useEffect, useState, KeyboardEvent } from "react"
 
 interface Props {
     content: CellContent;
 }
 
-export default function Cell({ content }: Props) {
+export default function Cell({ content: initialContent }: Props) {
+    const [editing, setEditing] = useState<boolean>(false)
+    const [content, setContent] = useState<CellContent>(initialContent)
+
+    const onKeyDown = (event: any) => {
+        if (["Enter", "Escape"].includes(event.key)) {
+            setEditing(false);
+        }
+    };
+
     return (
-        <td>{content}</td>
+        <td onClick={() => setEditing(!editing)}>
+            {editing ? (
+                <input
+                    onClick={(e) => e.stopPropagation()}
+                    onKeyDown={onKeyDown}
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
+                />
+            ) : (
+                content
+            )}
+        </td>
     )
 }
